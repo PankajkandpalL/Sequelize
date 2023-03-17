b5722876085ecaad8d2f3ba55fe3ddc1cd869d65
PankajkandpalL <pankajkandpal5602@gmail.com>
2023-03-17 11:53:57 +0530

    initialised with port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { seq } = require("./configs/dbConfig")
+// const { seq } = require("./configs/dbConfig")
 const express = require("express")
-const { UserRoute } = require("./Routes/User.route")
-const { orderRoute } = require("./Routes/Orders.route")
+// const { UserRoute } = require("./Routes/User.route")
+// const { orderRoute } = require("./Routes/Orders.route")
 const app = express()
 
 app.use(express.json())
@@ -10,8 +10,8 @@ app.get("/", (req,res)=>{
     res.send("Welcome To The Server")
 })
 
-app.use("/user", UserRoute)
-app.use("/orders",  orderRoute)
+// app.use("/user", UserRoute)
+// app.use("/orders",  orderRoute)
 
 app.listen(80, async()=>{
     // await seq.sync()
